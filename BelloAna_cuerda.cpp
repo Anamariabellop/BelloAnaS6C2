99c1cc9309c5ba2e5a1e7d1921c1236cb85eb195
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-09 11:31:54 -0500

    construccion condiciones generales

--- BelloAna_cuerda.cpp
+++ BelloAna_cuerda.cpp
@@ -4,31 +4,38 @@
 
 using namespace std;
 
-void eonda(float t0, float tf, int npuntos);
+void eonda(double const, double longitud,double t0, double tf, int npuntos);
 
 int main(){
 
-	float L=1.0;
-	float dx=0.01;
-	float c= 300.0;
+	double L=1.0;
+	double dx=0.01;
+	double c= 300.0;
 	int N = 100;
-	float dt=0.01;
-	float Aactual[N];
-	float Aold[N];
-	float Anew[N];
-	float s=c*(dt/dx);
-	float t =0.0;
-	float tfinal=0.1;
-	eonda(t,tfinal,N);
+	double t =0.0;
+	double tfinal=0.1;
+	eonda(c,L,t,tfinal,N);
 	return 0;
 }
 
-void eonda(float t0, float tf, int npuntos){
+void eonda(double con, double longitud,double t0, double tf, int npuntos){
 	ofstream outfile;
 	outfile.open("cuerda.txt");
 
-	float dx= (tf- t0)/(npuntos-1);
-	float tiempo[npuntos];
+	double Aactual[npuntos];
+	double Aold[npuntos];
+	double Anew[npuntos];
+	double A0=0.01;
+	//Aold[0]=0.0;
+	//A[npuntos-1]=0.0;
+	//A[(npuntos/2)-1]=A0;
+
+
+	double dx= (tf- t0)/(npuntos-1);
+	double dt=dt=(dx*0.1)/con ;
+	double s=con*(dt/dx);
+	double tiempo[npuntos];
+
 	tiempo[0]=t0;
 
 
@@ -39,7 +46,25 @@ void eonda(float t0, float tf, int npuntos){
 
     for(int i=0; i<npuntos; i++)
     {
-    	outfile  << tiempo[i] << endl;
+    	double m=(A0/(longitud/2));
+  		if( i==0){
+  			Aold[i]=0;
+  		}
+  		else if(i==npuntos-1){
+  			Aold[i]=0;
+  		}
+  		else if (i<(npuntos/2)-1)
+  		{
+  			Aold[i]= i*dx*m;
+  		}
+  		else if(i>(npuntos/2)-1){
+  			Aold[i]= -(i*dx*m)+(2*A0);
+  		}
+    }
+
+    for(int i=0; i<npuntos; i++)
+    {
+    	outfile  << tiempo[i]  << "  " << Aold[i] << endl;
     }
 	outfile.close();
 }
