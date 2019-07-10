88b11f6873cd67c662e9872b6ec793751ab57fa4
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-10 07:51:20 -0500

    avance

--- Plots_cuerda.py
+++ Plots_cuerda.py
@@ -5,9 +5,23 @@ datos=np.genfromtxt("cuerda.txt")
 
 t=datos[:,0]
 x=datos[:,2]
+xp=datos[:,3]
 
 plt.figure()
+plt.subplot(2,1,1)
 plt.plot(t,x)
+plt.xlabel("Amplitud")
+plt.ylabel(" ")
 plt.grid()
-#plt.ylim(-0.010,0.010)
+plt.ylim(-0.001,0.011)
+plt.title("Condiciones iniciales")
+
+plt.subplot(2,1,2)
+plt.plot(t,xp)
+plt.xlabel("Amplitud")
+plt.ylabel(" ")
+plt.grid()
+plt.ylim(-0.001,0.011)
+plt.title("Condiciones presente")
+plt.subplots_adjust(hspace=.5)
 plt.savefig("cuerda.png")
